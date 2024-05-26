4e2771028cd16d963e6a26cf9efced346666213d
Nathan Regner <nathanregner@gmail.com>
2024-05-26 11:20:25 -0600

    build: update to nixos-24.05

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   '';
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
